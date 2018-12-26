17c3ec465de29dccb39fadc08aaa24e9ffd8200d
541455 <541455@PC275422.cts.com>
2018-12-26 19:31:49 +0530

    ribbon load balancing

--- src/main/java/com/salesorder/microservices/SalesorderApplication.java
+++ src/main/java/com/salesorder/microservices/SalesorderApplication.java
@@ -2,6 +2,9 @@ package com.salesorder.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SalesorderApplication {
@@ -10,5 +13,10 @@ public class SalesorderApplication {
 		SpringApplication.run(SalesorderApplication.class, args);
 	}
 
+		@LoadBalanced
+		@Bean
+	  RestTemplate restTemplate() {
+	    return new RestTemplate();
+	  }
 }
 
